a908f4f2039dba7eea7b5ac6a389931dd0b4f87c
halosviel <guzman.carlo006@gmail.com>
2026-06-20 20:46:48 +0100

    added params

--- src/emitError.js
+++ src/emitError.js
@@ -1,5 +1,5 @@
 // just logs the error in console & in chat.
-module.export = function sendError() {
+module.export = function sendError(message, commandName, err) {
     console.error(`[${commandName}] cmd exec err: ${err}`);
     await message.channel.send("```diff\n❌️ COMMAND EXECUTION FAILURE\n- " + err + "```");
 }
