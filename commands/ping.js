b4735e70648bab1f4fa1b64441520a878c8e99c9
halosviel <guzman.carlo006@gmail.com>
2026-06-20 21:25:25 +0100

    reformatted options

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,11 @@
 module.exports = {
     name: "ping",
-    help: `
-Usage: ping [OPTIONS?]
-
-OPTIONS
-  -h  prints this help
-    `,
+    help: {
+        usage: "ping [OPTIONS?]",
+        options: [
+            ["h", "help", "prints this help"]
+        ]
+    },
     async run(message, _) {
         const now = Date.now();
         const ping = now - message.createdTimestamp;
