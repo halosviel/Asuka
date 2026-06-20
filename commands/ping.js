cc99d94070180b20ee6998e82ba72cbd430015a3
halosviel <guzman.carlo006@gmail.com>
2026-06-20 21:19:46 +0100

    added a help entry

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,11 @@
 module.exports = {
     name: "ping",
+    help: `
+Usage: ping [OPTIONS?]
+
+OPTIONS
+  -h  prints this help
+    `,
     async run(message, _) {
         const now = Date.now();
         const ping = now - message.createdTimestamp;
