dcc7b058ad131659f4278cd9fa6631c9176ff28c
halosviel <guzman.carlo006@gmail.com>
2026-06-20 21:03:20 +0100

    minor changes

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: "ping",
-    async run(message) {
+    async run(message, _) {
         const now = Date.now();
         const ping = now - message.createdTimestamp;
 
-        await message.channel.send(`pong~! In ${ping}ms`);
+        await message.channel.send(`pong~ in ${ping}ms!`);
     },
 };
